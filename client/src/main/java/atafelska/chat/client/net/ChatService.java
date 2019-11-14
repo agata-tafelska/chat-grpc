b55374129aea0f39558bef335a9b24f027bab035
Agata Tafelska <agata.tafelska95@gmail.com>
2019-11-15 00:13:15 +0100

    [Client] Implement ChatService

--- client/src/main/java/atafelska/chat/client/net/ChatService.java
+++ client/src/main/java/atafelska/chat/client/net/ChatService.java
@@ -1,29 +1,42 @@
 package atafelska.chat.client.net;
 
-import atafelska.chat.Chat;
-import atafelska.chat.ChatServiceGrpc;
-import atafelska.chat.User;
+import atafelska.chat.*;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 public class ChatService {
+    private static final int PORT = 9000;
 
-    private ChatServiceGrpc.ChatServiceBlockingStub blockingStub;
     private ChatServiceGrpc.ChatServiceStub asyncStub;
     private Channel channel;
 
-    public ChatService(String host, int port) {
-        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    public ChatService(String host) {
+        this(ManagedChannelBuilder.forAddress(host, PORT).usePlaintext());
     }
 
     private ChatService(ManagedChannelBuilder channelBuilder) {
         channel = channelBuilder.build();
-        blockingStub = ChatServiceGrpc.newBlockingStub(channel);
         asyncStub = ChatServiceGrpc.newStub(channel);
     }
 
-    public void getChat(User user) {
-        Chat chat = blockingStub.getChat(user);
+    public void getChat(User request, StreamObserver<Chat> responseObserver) {
+        asyncStub.getChat(request, responseObserver);
     }
 
+    public void unsubscribe(User request, StreamObserver<User> responseObserver) {
+        asyncStub.unsubscribe(request, responseObserver);
+    }
+
+    public void observeMessages(User request, StreamObserver<Message> responseObserver) {
+        asyncStub.observeMessages(request, responseObserver);
+    }
+
+    public void observeUsers(User request, StreamObserver<CurrentUsers> responseObserver) {
+        asyncStub.observeUsers(request, responseObserver);
+    }
+
+    public void sendMessage(Message request, StreamObserver<Message> responseObserver) {
+        asyncStub.sendMessage(request, responseObserver);
+    }
 }
