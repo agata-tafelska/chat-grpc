760b9216d78bf30de57f59f7010163ce0cdf282f
Agata Tafelska <agata.tafelska95@gmail.com>
2019-11-16 16:57:54 +0100

    [Client] Implement basic users and messages UI

--- client/src/main/java/atafelska/chat/client/controllers/ChatboardController.java
+++ client/src/main/java/atafelska/chat/client/controllers/ChatboardController.java
@@ -4,10 +4,12 @@ import atafelska.chat.Message;
 import atafelska.chat.User;
 import atafelska.chat.client.core.Logger;
 import atafelska.chat.client.utils.InputUtils;
+import javafx.application.Platform;
+import javafx.collections.ObservableListBase;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
 
 import java.util.List;
 import java.util.Observer;
@@ -19,9 +21,9 @@ public class ChatboardController extends BaseController {
     @FXML
     private Button buttonSend;
     @FXML
-    private VBox listMessages;
+    private ListView messagesListView;
     @FXML
-    private VBox listUsers;
+    private ListView usersListView;
     @FXML
     private TextArea editTextMessage;
 
@@ -29,16 +31,20 @@ public class ChatboardController extends BaseController {
     private Observer usersObserver =
             (observable, argument) -> {
                 Logger.print("Users update received, updating UI");
-                List users = ((List) argument);
-                updateUsers(users);
+                Platform.runLater(() -> {
+                    List users = ((List) argument);
+                    updateUsers(users);
+                });
             };
 
     @SuppressWarnings("unchecked")
     private Observer messagesObserver =
             (observable, argument) -> {
                 Logger.print("Messages update received, updating UI");
-                List messages = ((List) argument);
-                updateMessages(messages);
+                Platform.runLater(() -> {
+                    List messages = ((List) argument);
+                    updateMessages(messages);
+                });
             };
 
     @Override
@@ -54,18 +60,40 @@ public class ChatboardController extends BaseController {
         buttonSend.setOnAction(actionEvent -> sendMessage());
     }
 
+    @SuppressWarnings("unchecked")
     private void updateUsers(List<User> users) {
         Logger.print("Showing users");
-        users.forEach(
-                user -> Logger.print(user.toString())
-        );
+        ObservableListBase observableListBase = new ObservableListBase() {
+            @Override
+            public Object get(int index) {
+                return users.get(index).getName();
+            }
+
+            @Override
+            public int size() {
+                return users.size();
+            }
+        };
+
+        usersListView.setItems(observableListBase);
     }
 
+    @SuppressWarnings("unchecked")
     private void updateMessages(List<Message> messages) {
         Logger.print("Showing messages");
-        messages.forEach(
-                message -> Logger.print(message.toString())
-        );
+        ObservableListBase observableListBase = new ObservableListBase() {
+            @Override
+            public Object get(int index) {
+                return messages.get(index).getUser().getName() + ": " + messages.get(index).getText();
+            }
+
+            @Override
+            public int size() {
+                return messages.size();
+            }
+        };
+
+        messagesListView.setItems(observableListBase);
     }
 
     private void sendMessage() {
