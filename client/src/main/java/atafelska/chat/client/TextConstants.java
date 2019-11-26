65941773bf79e731dc9495d26eb5c15e4d216e5a
Agata Tafelska <agata.tafelska95@gmail.com>
2019-11-26 17:25:08 +0100

    [Client] Change application title to "ATChat"

--- client/src/main/java/atafelska/chat/client/TextConstants.java
+++ client/src/main/java/atafelska/chat/client/TextConstants.java
@@ -1,7 +1,7 @@
 package atafelska.chat.client;
 
 public class TextConstants {
-    public static final String TITLE_CHAT = "Chat";
+    public static final String TITLE_CHAT = "ATChat";
     public static final String INCORRECT_HOST_MESSAGE = "Incorrect host. Please try again.";
     public static final String INCORRECT_USERNAME_MESSAGE = "Username must contain at least 3 characters including upper- and lowercase,\n" +
             "numbers and \".\", \"-\", \"_\".";
