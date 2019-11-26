2cb7da89555b4a60f5069ffa9350c66905d9a817
Agata Tafelska <agata.tafelska95@gmail.com>
2019-11-26 17:27:59 +0100

    [Client] Add window icon

--- client/src/main/java/atafelska/chat/client/ClientApplication.java
+++ client/src/main/java/atafelska/chat/client/ClientApplication.java
@@ -1,8 +1,12 @@
 package atafelska.chat.client;
 
+import atafelska.chat.client.core.Logger;
 import atafelska.chat.client.core.SceneCoordinator;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
 
 public class ClientApplication extends Application {
     private SceneCoordinator sceneCoordinator;
@@ -10,8 +14,24 @@ public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setResizable(false);
-
+        initializeIcon(stage);
         sceneCoordinator = new SceneCoordinator(stage);
         sceneCoordinator.initApplication();
+    }
+
+    private void initializeIcon(Stage stage) {
+        try {
+            URL iconImageURL = ClassLoader.getSystemClassLoader().getResource("chatIcon.png");
+            if (iconImageURL == null) {
+                Logger.print("Initializing icon failed. Unable to retrieve icon URL.");
+                return;
+            }
+
+            Image iconImage = new Image(iconImageURL.openStream());
+            stage.getIcons().add(iconImage);
+        } catch (IOException exception) {
+            Logger.print("Initializing icon failed");
+            exception.printStackTrace();
+        }
     }
 }
