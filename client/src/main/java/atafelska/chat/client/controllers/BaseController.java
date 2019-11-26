a178938ff77d06f08e085982190ce76276a59f9f
Agata Tafelska <agata.tafelska95@gmail.com>
2019-11-26 22:47:16 +0100

    [Client] Implement possibility of initializing controllers with state
    This commit adds separate method, which makes possible to pass optional parameters to controller during initialization.

--- client/src/main/java/atafelska/chat/client/controllers/BaseController.java
+++ client/src/main/java/atafelska/chat/client/controllers/BaseController.java
@@ -2,6 +2,8 @@ package atafelska.chat.client.controllers;
 
 import atafelska.chat.client.core.SceneCoordinator;
 
+import java.util.Map;
+
 public abstract class BaseController {
     protected SceneCoordinator sceneCoordinator;
 
@@ -9,7 +11,19 @@ public abstract class BaseController {
         this.sceneCoordinator = sceneCoordinator;
     }
 
+    /*
+     Below method is called directly after Controller is loaded from FXML
+     Override it for initializing UI logic
+     */
     public void onLoaded() {
+
+    }
+
+    /*
+     Below method is called after above `onLoaded` only when optional parameters are passed to Controller
+     Override it when scene needs initial state stored in parameters
+     */
+    public void onOptionalParamsLoaded(Map<String, String> optionalParams) {
 
     }
 }
